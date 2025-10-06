00117cb7bf0d815b6b939ad46e32ce51a36b9cb2
Luciano Pompilio <lucianopompilio123@gmail.com>
2025-10-06 11:15:10 -0300

    add console log buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { CORE_CONCEPTS } from './data';
 import Header from './components/Header.jsx';
 import MainGoal from './components/MainGoal.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
 
   
 function App() {
-
+  let tabContent = 'Please click a button'
+  function  handleSelect (selectedButton) {
+    tabContent = selectedButton;
+    console.log(tabContent); 
+  }
   return (
     <div>
       <Header></Header>
@@ -19,6 +24,16 @@ function App() {
            <CoreConcepts {...CORE_CONCEPTS[3]} />
         </ul>
         </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('Jsx')}>Jsx</TabButton>
+            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
       <MainGoal></MainGoal>
     </div>
@@ -27,4 +42,3 @@ function App() {
 
 
 export default App;
- 
