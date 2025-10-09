cbe925c8b1269218b488f5fba95d73576d03d1ed
Luciano Pompilio <lucianopompilio123@gmail.com>
2025-10-09 12:36:30 -0300

    new dinamics bottons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header.jsx';
 import MainGoal from './components/MainGoal.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
-  
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState(null);
   let tabContent = 'Please click a button'
   function  handleSelect (selectedButton) {
-    tabContent = selectedButton;
-    console.log(tabContent); 
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic); 
   }
   return (
     <div>
@@ -27,12 +29,21 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('Jsx')}>Jsx</TabButton>
-            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>Jsx</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {tabContent}
+          {!selectedTopic && <p> Please select a topic.</p>}
+           {selectedTopic && (
+            <div id="tab-content"style={{ color: 'black' }}>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+          </div>
+          )}
         </section>
       </main>
       <MainGoal></MainGoal>
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 import "./index.css";
-
+import "./components/Header.css";
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
